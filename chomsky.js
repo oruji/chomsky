7203757078c4f952a372c38d954bb99a9e84d047
Amin Oruji <amin@oruji.org>
2020-03-03 11:59:40 +0330

    add essential files

--- chomsky.js
+++ chomsky.js
@@ -1,9 +1,18 @@
-function minimize(str) {
-  return toHier(str);
-}
+var types = {
+  ADD : 'add',
+  MUL : 'mul',
+  STAR : 'star',
+  ATOM : 'atom',
+  LAM : 'lam'
+};
 
+var specs = {
+  LAMBDA : '\u03BB'
+};
+
+// convert String to Hierarchical Structure
 function toHier(str) {
-  // convert str to array
+  // convert string to array
   var arr = [];
   for ( var i = 0; i < str.length; i++) {
     arr.push(str[i]);
@@ -11,9 +20,60 @@ function toHier(str) {
 
   // generate hierarchical structure
   if (arr.length === 0) {
-    return {
-      type : "multiplication",
-      elms : []
-    };
+    return genMul([]);
   }
+
+  var globArr = makeInputSeq(arr);
+  var hier = firstComp(globArr);
+
+  return hier;
+}
+
+function select() {
+  return this.arr[this.index];
+}
+function next() {
+  ++this.index;
+}
+function makeInputSeq(arr) {
+  return {
+    arr : arr,
+    index : 0,
+    select : select,
+    next : next
+  };
+}
+
+function genAdd(sels) {
+  return {
+    type : types.ADD,
+    items : sels
+  };
+}
+
+function genMul(els) {
+  return {
+    type : types.MUL,
+    items : els
+  };
+}
+
+function genStar(expr) {
+  return {
+    type : types.STAR,
+    item : expr
+  };
+}
+
+function genAtom(atom) {
+  return {
+    type : types.ATOM,
+    item : atom
+  };
+}
+
+function genLam() {
+  return {
+    type : types.LAM
+  };
 }
